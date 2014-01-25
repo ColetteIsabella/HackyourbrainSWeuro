93a6413c2bf05882d3f54007fa2c9b79e1f34a9f
Colette Isabella <coletteisabella.com>
2014-01-25 14:05:00 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackYourBrain::Application.config.secret_key_base = 'b6c80fe22652b3b65ac95c1ada68e9764bb069e86c8b92fba5e79e6dd049409c7929c43d2953c606abe366fbb7dbb6ca2fbe9ae9462d387a7a0ec22a92663d31'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackYourBrain::Application.config.session_store :cookie_store, key: '_hack_your_brain_session'
