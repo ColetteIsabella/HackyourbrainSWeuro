444b27ab6cc66b18bfb5e2e892f755efa2c87817
erikadeal <erika.deal@gmail.com>
2014-01-25 19:57:03 -0800

    Challenge page structure

--- static/js/global.js
+++ static/js/global.js
@@ -5,3 +5,17 @@ $(document).ready (
 			$("nav").slideToggle();
 		});
 	});
+
+$(document).ready (
+	function() {
+		$(document)on.("click", ".add-button", function(){
+			$("#challenges").append("					
+				<div class="grid__item one-half">
+							<input type="text">
+					</div><!--
+				 --><div class="grid__item one-half">
+				 			<input type="textarea">
+					</div>");
+		});
+
+	});
